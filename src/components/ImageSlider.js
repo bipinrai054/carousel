7e04810d8b0c7148081f588373d9d2ae5e463ecb
bipnrai054 <bipinrai054@gmail.com>
2021-05-23 17:35:31 +0545

    finished

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,13 +1,43 @@
 import React, { useState } from 'react';
 import { SliderData } from './SliderData';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
-const ImageSlider = () => {
+const ImageSlider = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  // if (!Array.isArray(slides) || slides.length <= 0) {
+  //   return null;
+  // }
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+  console.log(current);
+
   return (
-    <>
+    <section className='slider'>
+      <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
+      <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
       {SliderData.map((slide, index) => {
-        return <img src={slide.image} alt='candies' key={index} />;
+        return (
+          <div className={index === current ? 'slide active' : 'slide'}>
+            {index === current && (
+              <img
+                src={slide.image}
+                alt='candies'
+                key={index}
+                className='image'
+              />
+            )}
+          </div>
+        );
       })}
-    </>
+    </section>
   );
 };
 
